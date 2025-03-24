bd35ad19cfbc718932b922c77ccb7fed49de3ff1
Janus <Janus.Haung@hualiteq.com>
2025-03-24 15:14:16 +0800

    Alighed the context to center

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,7 @@ class MyHomePage extends StatelessWidget {
 
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
 // MARK: Text 
           Text('A random idea - we testing hot reload:'),
